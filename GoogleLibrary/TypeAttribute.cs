b541d13e1949e0f809c0a567fbf50ab8e482074a
Gareth Bird <garethbird@gmail.com>
2024-06-02 11:03:44 +0100

    type attribute

--- GoogleLibrary/TypeAttribute.cs
+++ GoogleLibrary/TypeAttribute.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using PostSharp.Patterns.Contracts;
+using System;
 
 namespace GoogleLibrary
 {
     [AttributeUsage(AttributeTargets.All, Inherited = false)]
-    public class TypeAttribute(Type type) : Attribute
+    public class TypeAttribute([Required] Type type) : Attribute
     {
         public Type Type { get; } = type;
     }
